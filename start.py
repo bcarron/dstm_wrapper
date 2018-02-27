c367bc8eb8f516afedaa1fc4143c7bee079827f3
Blaise <blaise.carron@etu.unige.ch>
2018-02-27 03:13:25 +0100

    Add log to the wrapper

--- start.py
+++ start.py
@@ -4,27 +4,31 @@ import argparse
 import subprocess
 import sys
 import os
+import logging
 
 def restart_miner(proc):
-    print('Restarting miner...')
+    logging.info('Restarting miner...')
     proc.kill()
 
 def process_line(args, proc, line):
     if 'cudaMemcpy 1 failed' in line:
         restart_miner(proc)
     if args.reboot and 'cudaGetDeviceCount failed' in line:
-        print('Restarting system...')
+        logging.info('Restarting system...')
         os.system('reboot')
     else:
-        print(line, end='')
+        logging.info(line)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Restart dstm's miner when an overclocking error is detected")
     parser.add_argument('-l', '--launch_command', default='./zm --cfg-file zm.cfg', help="the command used to launch dstm's miner")
     parser.add_argument('-r', '--reboot', action='store_true', help="restart the machine if a 'cudaGetDeviceCount failed' error is encountered (requires root privileges)")
     args = parser.parse_args()
+
+    logging.basicConfig(filename='zm_wrapper.log', level=logging.INFO, format='%(message)s')
+    logging.getLogger().addHandler(logging.StreamHandler())
     
     while True:
         proc = subprocess.Popen(args.launch_command.split(' '), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         for line in proc.stdout:
-            process_line(args, proc, line.decode())
+            process_line(args, proc, line.decode().strip('\n'))
